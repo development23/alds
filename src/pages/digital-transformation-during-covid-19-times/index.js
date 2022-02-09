14ee9c7ace3fa4240aa7f049dfd19de872ff2ac0
purushottam-aladinntech <95672599+purushottam-aladinntech@users.noreply.github.com>
2022-02-09 17:27:53 +0530

    9feb

---
+++ src/pages/digital-transformation-during-covid-19-times/index.js
@@ -0,0 +1,187 @@
+import React from "react";
+import Image from "next/dist/client/image";
+import Sidebar from "components/sidebar";
+import styles from "./styles.module.css";
+
+export default function blog1() {
+  return (
+    <div>
+      <div
+        className={` ${styles.mts} flex flex-wrap  fixed z-0 overflow-hidden `}
+      >
+        <div className="w-full pt-6 pl-8 pr-6 overflow-hidden   xl:w-1/5">
+          <Sidebar />
+        </div>
+
+        <div className={`  ${styles.btp} w-full overflow-hidden    xl:w-4/5 `}>
+          <Image
+            src={require("@/assets/images/blogs/banner/blog1.jpg")}
+            alt="Aladinn Tech"
+          />
+        </div>
+      </div>
+
+      <div className={` ${styles.mtsin} relative bg-white pb-8`}>
+        <div className="md:px-28 md:pt-10 ">
+          <div className="flex flex-wrap md:px-38  overflow-hidden ">
+            <div
+              className={` ${styles.innerpagecon} w-full  pt-6 pl-8 pr-6 overflow-hidden md:w-full `}
+            >
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> 08 Feb 2021 </p>
+
+                <h2 className={` ${styles.titlemain} `}>
+                  {" "}
+                  Digital Transformation during Covid 19 Times
+                </h2>
+              </div>
+
+              <p>
+                {" "}
+                The notion and application of Digital connectivity in the times
+                of COVID-19 have made a massive shift. It is no longer about
+                traditional communication and searching for information. It is
+                more about appropriate use of data, consumption of vast amounts
+                of content available on the internet, and engagement of
+                individuals, governments, and businesses in digital applications
+                to ensure continuity of economic and social activities amidst
+                lockdowns imposed all over the world which has disrupted normal
+                human activities, most importantly quality interpersonal
+                interactions. The inclusion of digital technologies has brought
+                fundamental transformations in almost all the sectors of the
+                country, ranging from education, health, government services to
+                financial private services.
+              </p>
+              <p>
+                {" "}
+                As the top Web and Mobile App Development Company in Jaipur,
+                ALDS has developed various web applications and solutions during
+                Covid 19.
+              </p>
+            </div>
+
+            <div
+              className={` ${styles.innerpagecon} w-full  pt-6 pl-8 pr-6 overflow-hidden md:w-full `}
+            >
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> Education Sector</p>
+              </div>
+              <p>
+                {" "}
+                The transformation of higher education after the inception of
+                Covid 19 is worth a watch. Digitalization of classrooms which
+                involves online interaction of teachers and students, use of AI
+                Technologies in studies and assessments of education systems,
+                including use of real-time big data, dependence on web 2.0 tools
+                and technologies for delivering knowledge to students, and
+                creation of virtual reality are some of the features of
+                transformed education system post-Covid 19.
+              </p>
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> Health Sector </p>
+              </div>
+              <p>
+                During the pandemic, one can witness many examples of digital
+                health evolving. Companies like Apple and Google came together
+                to build a contact- Tracing system for patients. AI-powered
+                sensors are helping crowds to identify potentially contagious
+                individuals. Robots are monitoring patients and taking over
+                higher-risk care tasks such as providing supplies. Physicians
+                are using patient-friendly digital tools for routine monitoring
+                and diagnosis in multiple diseases. Data mining techniques have
+                been employed in studying and assessing diseases of chronic
+                nature like pulmonary hypertension. Patient-centric platforms
+                have been created to improve the patient recovery journey.
+              </p>
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> Financial Sector </p>
+              </div>
+              <p>
+                {" "}
+                Post-Covid 19, companies have changed the pattern of working.
+                They are leveraging data to provide better customer experiences,
+                whether it’s an online shopping site that uses technologies like
+                artificial intelligence and machine learning or a retailer that
+                uses a mobile app to offer customers contactless payment.
+                Automation and machine learning can bring efficiencies in
+                routine tasks, while other technologies like augmented reality
+                can assist workers in ways not possible earlier. Fueled by the
+                concepts like the Internet of Things and Industry 4.0,
+                innovations like Digital Twins help companies manage real-time
+                data to bring efficiency in operations and encourage innovations
+                in the system.{" "}
+              </p>
+              <p>
+                {" "}
+                <strong>
+                  {" "}
+                  ALDS, one of the top Native mobile application development
+                  companies, post-Covid 19 has designed solutions with the
+                  following features:-{" "}
+                </strong>
+              </p>
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}>
+                  {" "}
+                  EFFECTIVE PERFORMANCE{" "}
+                </p>
+              </div>
+              <p>
+                {" "}
+                The mobile app solutions offer an extremely high level of
+                performance. Fast and responsive, the app is much more
+                efficient. It is user compliant and when the user navigates
+                through the mobile app, the contents and visual elements are
+                already stored on their phone which means load times are quick.
+              </p>
+
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}>MORE SECURE </p>
+              </div>
+              <p>
+                {" "}
+                The mobile app provides a great way to guarantee your users
+                reliable data protection.
+              </p>
+
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> MORE INTERACTIVE </p>
+              </div>
+              <p>
+                {" "}
+                More smooth to use and its advantage amounts to superior user
+                experience with a specific operating system. As a result, the
+                flow of the app is more natural.
+              </p>
+
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}>
+                  {" "}
+                  ACCESS TO FULL FEATURE SET OF DEVICES{" "}
+                </p>
+              </div>
+              <p>
+                {" "}
+                xDirect and easy access to the hardware of the device such as
+                the GPS, camera, microphone, etc. so they are faster in
+                execution, which ultimately results in a better user experience.
+                Push notifications are an added advantage.
+              </p>
+
+              <div className={` ${styles.titlesubcontainer} `}>
+                <p className={` ${styles.titlesub} `}> FEWER BUGS </p>
+              </div>
+              <p>
+                {" "}
+                The Native apps have been built with the most recent features.
+                Because of this, users have access to new platform features once
+                they update the operating system, unlike hybrid app development,
+                which amounts to a frustrating user experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/our-blogs/index.jsx
+++ src/pages/our-blogs/index.jsx
@@ -2,28 +2,10 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Sidebar from "components/sidebar";
 import Image from "next/image";
-import styles from "../../pages/about-us/styles.module.css";
-import WbSunnyIcon from "@material-ui/icons/WbSunny";
-import BrushIcon from "@material-ui/icons/Brush";
-import DeveloperModeIcon from "@material-ui/icons/DeveloperMode";
-import LaptopMacIcon from "@material-ui/icons/LaptopMac";
-import ShowChartIcon from "@material-ui/icons/ShowChart";
-import SearchIcon from "@material-ui/icons/Search";
-import GroupsIcon from "@material-ui/icons/Group";
-import WorkIcon from "@material-ui/icons/Work";
-import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
-import PersonPinCircleIcon from "@material-ui/icons/PersonPinCircle";
-import StarsSharpIcon from "@material-ui/icons/StarsSharp";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import InstallMobileIcon from "@material-ui/icons/MobileFriendly";
-import ComputerIcon from "@material-ui/icons/Computer";
-import ViewCompactIcon from "@material-ui/icons/ViewCompact";
-import ViewComfyIcon from "@material-ui/icons/ViewComfy";
+import styles from "./styles.module.css";
 
 //Tabs
 import PropTypes from "prop-types";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
@@ -77,30 +59,13 @@ export default function Blogs() {
   const ourvalues = [
     {
       id: 0,
-      image: require("@/assets/images/home/client/1.png"),
+      image: require("@/assets/images/blogs/card/1.jpeg"),
 
-      title: "Mahatma Gandhi Hospital",
-
-      description:
-        "The development team at ALDS is exceptionally outstanding and cooperative. They understand every detailed requirement for website and app development and suggest the necessary alternatives wherever possible to develop an engaging product that can give new heights to your business in the direction that you want. ",
-    },
-    {
-      id: 1,
-      image: require("@/assets/images/home/client/6.png"),
-
-      title: "Arya Group of Colleges",
+      title: "Digital Transformation during Covid 19 Times      ",
 
       description:
-        "The tech team of ALDS is well versed with all the latest and best technologies. Their quality approach, exceptional service, and complete technical support are really admirable. The entire team is committed and works round the clock to fulfill the client’s requirements. Thanks, Team ALDS! ",
-    },
-    {
-      id: 2,
-      image: require("@/assets/images/home/client/12.png"),
-
-      title: "Apex Hospital",
-
-      description:
-        "We are highly satisfied with the development services of ALDS. The highly functional and responsive website provides the best user experience. They have also developed an exceptional telemedicine platform for us and continue to support us in making it even better. This is all that we were looking for. The web solutions of the company are recommendable. Keep doing the great work guys! ",
+        "As the top Web and Mobile App Development Company in Jaipur, ALDS has developed various web applications and solutions during Covid 19.        ",
+      link: "digital-transformation-during-covid-19-times",
     },
   ];
   return (
@@ -114,7 +79,7 @@ export default function Blogs() {
 
         <div className={`  ${styles.btp} w-full overflow-hidden    xl:w-4/5 `}>
           <Image
-            src={require("@/assets/images/banner/coming.jpg")}
+            src={require("@/assets/images/banner/blog.jpg")}
             alt="Aladinn Tech"
           />
         </div>
@@ -123,42 +88,49 @@ export default function Blogs() {
       <div className={` ${styles.mtsin} relative bg-white `}>
         <div className="md:px-28 md:pt-10 md:pb-28 ">
           <div className="flex flex-wrap md:px-38  overflow-hidden ">
-            {/* <div
+            <div
               className={` ${styles.innerpagecon} w-full  pt-6 pl-8 pr-6 overflow-hidden md:w-full `}
             >
               <div className={` ${styles.titlesubcontainer} `}>
                 <p className={` ${styles.titlesub} `}>
                   {" "}
-                  Discover What Our Clients Have To Share{" "}
+                  Read our blogs to get the latest updates of tech world.
                 </p>
-
-                <h2 className={` ${styles.titlemain} `}> Testimonials </h2>
+                <h2 className={` ${styles.titlemain} `}> Our Blogs </h2>
               </div>
-            </div> */}
+            </div>
 
-            {/* <div className={` pt-20 md:pt-10 md:pb-10   `}>
+            <div className={` pt-8 md:pt-10 md:pb-10   `}>
               <div className="flex flex-wrap overflow-hidden ">
                 {ourvalues.map((item, index) => (
                   <div
-                    className="my-1 px-2 text-center w-full sm:w-1/3 md:w-1/2 overflow-hidden"
+                    className="my-1 px-2 text-center w-full   md:w-1/2 overflow-hidden"
                     key={index}
                   >
                     <div
-                      className={`w-full overflow-hidden wow fadeInUp ${styles.singleservices} `}
+                      className={`  wow fadeInUp ${styles.singleservices}  `}
                       data-wow-delay="0.2s"
                     >
                       <Image src={item.image} alt={item.title} />
                       <h5 className="font-bold text-center text-gray-600 text-xl mb-3">
                         {item.title}
                       </h5>
-                      <p className="font-normal text-center text-base my-2">
+                      <p className="font-normal text-center text-base my-2 mb-8">
                         {item.description}
                       </p>
+
+                      <a
+                        href={item.link}
+                        className="px-4 py-4 bg-[#066da1] rounded-md mt-4 text-white"
+                      >
+                        {" "}
+                        View More
+                      </a>
                     </div>
                   </div>
                 ))}
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
